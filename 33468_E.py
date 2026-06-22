3851e86fee7058ae34041a1b1ec0e1ebe1f97f63
xim2016 <xj_ma@yahoo.com>
2026-06-22 16:09:27 -0400

    Update 33468_E.py

--- 33468_E.py
+++ 33468_E.py
@@ -22,7 +22,8 @@ div[class*="stSelectbox"] label { color: purple; }
 """
 st.write(tabs_font_css, unsafe_allow_html=True)
 
-c1, c2 = st.columns([2, 1])
+c1,_, c2,_ = st.columns([2, 0.2, 0.96, 0.02])
+
 
 tf_option = c1.selectbox(label='TF', options=tf_names)
 marker_option = c2.selectbox(label='mRNA Marker', options=marker_names)
