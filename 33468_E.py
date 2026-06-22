5a19d0aebf32166e1d80fabb89ba2abdb7975ae3
xim2016 <xj_ma@yahoo.com>
2026-06-22 14:48:12 -0400

    Update 33468_E.py

--- 33468_E.py
+++ 33468_E.py
@@ -12,6 +12,9 @@ session_id_ct = f"{sample}_ct_names"
 tf_names = st.session_state[session_id_tf]
 tf_names = sorted(tf_names)
 
+marker_names = st.session_state[session_id_marker]
+marker_names = sorted(marker_names)
+
 ct_names = st.session_state[session_id_ct]
 ct_names = sorted(ct_names)
 
@@ -27,22 +30,32 @@ div[class*="stSelectbox"] label {
 
 st.write(tabs_font_css, unsafe_allow_html=True)
 
-option = st.selectbox(
+c1,c2 = st.columns([2, 1])
+
+option = c1.selectbox(
     label='TF',
     options=tf_names,
     ) 
+option = c2.selectbox(
+    label='mRNA Marker',
+    options=marker_names,
+    ) 
 
-imgfile = f"./data/{sample}/spatial_tf/{option}_(TF_activity_and_mRNA_expression).png"
-# st.image(imgfile)
 import base64
 from pathlib import Path
-img_b64 = base64.b64encode(Path(imgfile).read_bytes()).decode()
-st.markdown(f"""
-    <div style="display: flex; justify-content: center;">
-        <img src="data:image/png;base64,{img_b64}" style="max-width: 100%; width: 800px;">
-    </div>
-""", unsafe_allow_html=True)
 
+imgfile = f"./data/{sample}/spatial_tf/{option}_(TF_activity_and_mRNA_expression).png"
+c1.image(imgfile)
+
+# img_b64 = base64.b64encode(Path(imgfile).read_bytes()).decode()
+# c1.markdown(f"""
+#     <div style="display: flex; justify-content: center;">
+#         <img src="data:image/png;base64,{img_b64}" style="max-width: 100%; width: 800px;">
+#     </div>
+# """, unsafe_allow_html=True)
+
+imgfile = f"./data/{sample}/spatial_tf/{option}__mRNA_expression.png"
+c2.image(imgfile)
 
 st.write("")
 option = st.selectbox(
